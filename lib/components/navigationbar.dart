e131f10c90575ea1108006719bccb4649b2f9ac9
AlleeElias <eliasneel2001@gmail.com>
2023-02-20 17:01:54 +0100

    created navbar buttons

--- lib/components/navigationbar.dart
+++ lib/components/navigationbar.dart
@@ -10,8 +10,15 @@ class NavigationWidget extends StatefulWidget {
 class _NavBarState extends State<NavigationWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return BottomNavigationBar(items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.account_balance), label: 'Posts'),
+      BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Messages')
+    ]);
+  }
+/*    return Container(
         decoration: const BoxDecoration(color: Colors.white),
         child: const Text('Dit is de navigatiebar?'));
-  }
+  }*/
 }
