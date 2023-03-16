3b74a891ee67acad2cee3e0aa03e4001153b3ec1
AlleeElias <eliasneel2001@gmail.com>
2023-03-16 13:12:54 +0100

    formatting cards

--- lib/components/postCard.dart
+++ lib/components/postCard.dart
@@ -18,18 +18,30 @@ class _PostCardState extends State<PostCard> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(children: [
-      Row(
-        children: [Text(p.title)],
-      ),
-      Row(
-        children: [
-          Expanded(
-            flex: 10,
-            child: Image.asset(p.image),
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(10)),
+        padding: const EdgeInsets.all(5.0),
+        margin: const EdgeInsets.all(5.0),
+        child: Column(children: [
+          Row(
+            children: [
+              Text(
+                p.title,
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 10,
+                child: Image.asset(p.image),
+              )
+            ],
           )
-        ],
-      )
-    ]);
+        ]));
   }
 }
