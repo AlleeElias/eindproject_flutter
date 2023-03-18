307797ab06d053f0de5d6332dd67ff3c3521fb87
AlleeElias <eliasneel2001@gmail.com>
2023-03-18 12:36:13 +0100

    video added

--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -1,6 +1,7 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -30,27 +31,51 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       body: Center(
           child: Column(
-        children: [
-          Row(
             children: [
-              Expanded(
-                child: Image.asset("images/profilepic.jpeg"),
-                flex: 6,
+              Row(
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: GestureDetector(
+                        onTap: () =>
+                        {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => VideoPage()))
+                        },
+                        child: Image.asset("images/profilepic.jpeg")),
+                  ),
+                  Expanded(
+                    child: Text(
+                        "Sneeuwberen zijn cool. Geniet van onze sexy beren geluiden! Je kan ook op de foto klikken om een mooie berenvideo te zien :)"),
+                    flex: 4,
+                  )
+                ],
               ),
-              Expanded(
-                child: Text(
-                    "Sneeuwberen zijn cool. Geniet van onze sexy beren geluiden!"),
-                flex: 4,
+              Row(
+                children: [
+                  Text("Deel jouw mening over deze prachtige beren hieronder:")
+                ],
               )
             ],
-          ),
-          Row(
-            children: [
-              Text("Deel jouw mening over deze prachtige beren hieronder:")
-            ],
-          )
-        ],
-      )),
+          )),
     );
+  }
+}
+
+class VideoPage extends StatelessWidget {
+  VideoPlayerController videoPlayerController =
+  VideoPlayerController.asset("assets/videos/beelden.mp4");
+
+  @override
+  Widget build(BuildContext context) {
+    videoPlayerController.initialize();
+    videoPlayerController.play();
+    return GestureDetector(
+        onDoubleTap: () => {
+        Navigator.pop(context)
+    },
+        child: VideoPlayer(videoPlayerController));
   }
 }
